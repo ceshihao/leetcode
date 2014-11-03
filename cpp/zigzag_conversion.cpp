6c00921f9ed2444d1c0bed21c520eae8b7354ea5
ceshihao <davidzheng23@gmail.com>
2014-11-03 20:58:20 +0800

    commit zigzag_conversion.cpp

---
+++ cpp/zigzag_conversion.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        int length = s.size();
+        string result;
+        for (int i=0; i<nRows; ++i)
+        {
+            string tmp;
+            int j=i;
+            while (j<length)
+            {
+                if(j%(nRows-1) == 0)
+                {
+                    tmp += s[j];
+                    j += 2*(nRows-1);
+                }
+                else
+                {
+                    tmp += s[j];
+                    if (j+2*(nRows-1)-2*i<length)
+                        tmp += s[j+2*(nRows-1)-2*i];
+                    j += 2*(nRows-1);
+                }
+            }
+            result+= tmp;
+        }
+        return result;
+    }
+};
