c99ccffd68d7da99bd3433fcc985537949951d01
Zheng Dayu <davidzheng23@gmail.com>
2019-03-14 18:42:39 +0800

    add 2 golang solutions

---
+++ golang/11.container-with-most-water.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=11 lang=golang
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (42.37%)
+ * Total Accepted:    327.6K
+ * Total Submissions: 763K
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ *
+ * Note: You may not slant the container and n is at least 2.
+ *
+ *
+ *
+ *
+ *
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49.
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ *
+ */
+
+func maxArea(height []int) int {
+	area := 0
+	length := len(height)
+	for left, right := 0, length-1; left < right; {
+		area = max(area, (right-left)*min(height[left], height[right]))
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return area
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
---
+++ golang/61.rotate-list.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=61 lang=golang
+ *
+ * [61] Rotate List
+ *
+ * https://leetcode.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (26.41%)
+ * Total Accepted:    179.9K
+ * Total Submissions: 676.7K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, rotate the list to the right by k places, where k is
+ * non-negative.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1->2->3->4->5->NULL, k = 2
+ * Output: 4->5->1->2->3->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 5->1->2->3->4->NULL
+ * rotate 2 steps to the right: 4->5->1->2->3->NULL
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 0->1->2->NULL, k = 4
+ * Output: 2->0->1->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 2->0->1->NULL
+ * rotate 2 steps to the right: 1->2->0->NULL
+ * rotate 3 steps to the right: 0->1->2->NULL
+ * rotate 4 steps to the right: 2->0->1->NULL
+ *
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil || k == 0 {
+		return head
+	}
+	cur, cur2nd := head, head
+	length := 0
+	for cur.Next != nil {
+		length++
+		cur = cur.Next
+		if length > k {
+			cur2nd = cur2nd.Next
+		}
+	}
+	if length < k {
+		return rotateRight(head, k%(length+1))
+	}
+	cur.Next = head
+	head = cur2nd.Next
+	cur2nd.Next = nil
+	return head
+}
+
